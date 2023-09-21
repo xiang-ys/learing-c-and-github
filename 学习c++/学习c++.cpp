bf2081ca058ef81c78f711e4f03c8c2a6c88b3c3
xiang-ys <525717458@qq.com>
2023-09-21 23:20:32 +0800

    添加删除数据

--- 学习c++/学习c++.cpp
+++ 学习c++/学习c++.cpp
@@ -1,107 +1,4 @@
-﻿//// 学习c++.cpp
-//#include <iostream>
-//#include<stdio.h>
-//
-//using namespace std;
-//
-//int add(int x, int y) {
-//	return x + y;
-//}
-//
-//int main()
-//{
-//	int a = 10;
-//	int b = 20;
-//	int sum = add(a, b);
-//	cout << a << "与" << b << "的和为" << sum;
-//	return 0;
-//}
-//
-//
-//void swap(int& a, int& b) {
-//	int temp = 0;
-//	temp = a;
-//	a = b;
-//	b = temp;
-//}
-//
-//void swap2(int* a, int* b) {
-//	int temp = 0;
-//	temp = *a;
-//	*a = *b;
-//	*b = temp;
-//}
-//
-//int main() {
-//	int a, b, c, d;
-//	printf("输入两个数\n");
-//	scanf_s("%d %d", &a, &b);
-//	swap(a, b);
-//	printf("交换后的结果为%d, %d", a, b);
-//	printf("再输入两个数\n");
-//	scanf_s("%d %d",&c,&d);
-//	swap2(&c, &d);
-//	printf("交换后的结果为%d, %d", c, d);
-//}
-//
-//
-//typedef struct fushu {
-//	float re;
-//	float mag;
-//};
-//fushu add(fushu a,fushu b) {
-//	fushu sum;
-//	sum.re = a.re + b.re;
-//	sum.mag = a.mag + b.mag;
-//	printf("相加后的复数为%.2f+%.2fi\n", sum.re, sum.mag);
-//	return sum;
-//}
-//fushu jian(fushu a, fushu b) {
-//	fushu sum;
-//	sum.re = a.re - b.re;
-//	sum.mag = a.mag - b.mag;
-//	printf("相减后的复数为%.2f+%.2fi\n", sum.re, sum.mag);
-//	return sum;
-//}
-//fushu cheng(fushu a, fushu b) {
-//	fushu sum;
-//	sum.re = (a.re * b.re) - (a.mag * b.mag);
-//	sum.mag = (a.mag * b.re) + (a.re * b.mag);
-//	printf("相乘后的复数为%.2f+%.2fi\n", sum.re, sum.mag);
-//	return sum;
-//}
-//fushu chu(fushu a, fushu b) {
-//	fushu sum;
-//	sum.re = ((a.re * b.re) + (a.mag * b.mag))
-//		/ ((b.re) * (b.re) + (b.mag) * (b.mag));
-//	sum.mag = ((a.mag * b.re) - (a.re) * (b.mag))
-//		/ ((b.re) * (b.re) + (b.mag) * (b.mag));
-//	printf("相除后的复数为%.2f+%.2fi\n", sum.re, sum.mag);
-//	return sum;
-//}
-//void printffushu(fushu a) {
-//	printf("%.2f+%.2fi\n", a.re, a.mag);
-//}
-//int main()
-//{
-//	fushu f1,f2;
-//	f1.re = 12.34;
-//	f1.mag = 45.23;
-//	f2.re = 25.36;
-//	f2.mag = 12.56;
-//	fushu sum;
-//	fushu shang;
-//	fushu ji;
-//	fushu cha;
-//	printffushu(f1);
-//	printffushu(f2);
-//	sum = add(f1, f2);
-//	cha = jian(f1, f2);
-//	ji = cheng(f1, f2);
-//	shang = chu(f1, f2);
-//	return 0;
-//}
-
+﻿//// 学习数据库c++.cpp
 #include<stdlib.h>
 #include<iostream>
 #define ElemType int    //初始化数据类型 
@@ -116,6 +13,35 @@ typedef struct {
 	int listsize; //最大长度 
 }SqList;
 
+int InitList(SqList& L);//线性表初始化
+void input(SqList& L, int num);//添加数据 ,添加次数为num
+void ListOutput(SqList& L);//输出数据
+void InsertData(SqList& L, int location, int num);//插入(Insert)数据
+void DeleteData(SqList& L, int location);//删除数据
+
+int main() {
+	int num;//数字
+	int location;//位置
+	SqList L1;//定义线性表
+	InitList(L1);//初始化
+	cout << "请输入你要添加多少个数据进去，不得超过100" << endl;
+	cin >> num;//你要添加多少个数据进去
+	input(L1, num);//添加数据
+	ListOutput(L1);//输出数据
+
+	cout << "现在执行插入数据处理，输入你要插入的位置和数据，空格隔开" << endl;
+	cin >> location >> num;
+	InsertData(L1, location, num);
+	ListOutput(L1);//输出数据
+
+	cout << "现在执行删除数据处理，输入你要删除的位置" << endl;
+	cin >> num;
+	DeleteData(L1, num);
+	ListOutput(L1);//输出数据
+	return 0;
+}
+
+
 int InitList(SqList& L) {//线性表初始化 
 	L.elem = (ElemType*)malloc(listsize_MAX * sizeof(ElemType));//malloc需要 #include<stdlid.h>
 	if (!L.elem)exit(0);//指针为NULL相当于指针等于零，所以L.elem==NULL则!L.elem为真
@@ -124,7 +50,7 @@ int InitList(SqList& L) {//线性表初始化
 	return ok;
 }
 
-void add(SqList& L, int num) {//添加数据 ,添加次数为num 
+void input(SqList& L, int num) {//添加数据 ,添加次数为num
 	L.length = num;
 	for (int i = 0; i < L.length; i++) {//一个一个添加
 		cout << "连续输入" << num << "个数字加入数据库,每次只输入一个" << endl;
@@ -142,25 +68,15 @@ void ListOutput(SqList& L) {//输出数据
 
 void InsertData(SqList& L,int location,int num) {//插入(Insert)数据
 	L.length++;
-	for (int i = L.length; i > location; i--) {//在位置(location)后的数据全部往后移一位
-		L.elem[i - 1] = L.elem[i - 2];
+	for (int i = L.length - 1; i >= location; i--) {//在位置(location)后的数据全部往后移一位
+		L.elem[i] = L.elem[i - 1];
 	}
-	L.elem[location - 1] = num;//插入数据
+	L.elem[location - 1] = num;//插入数据,减一是因为以位置开始数，不是索引
 }
 
-int main() {
-	int num;//数字
-	int location;//位置
-	SqList L1;//定义线性表
-	InitList(L1);//初始化
-	cout << "请输入你要添加多少个数据进去，不得超过100" << endl;
-	cin >> num;//你要添加多少个数据进去
-	add(L1, num);//添加数据
-	ListOutput(L1);//输出数据
-
-	cout << "现在执行插入数据处理，输入你要插入的位置和数据，空格隔开" << endl;
-	cin >> location >> num;
-	InsertData(L1, location, num);
-	ListOutput(L1);//输出数据
-	return 0;
+void DeleteData(SqList& L,int location) {//删除数据
+	L.length--;
+	for (int i = location; i <=L.length; i++) {//在位置(location)后的数据全部往前移一位
+		L.elem[i - 1] = L.elem[i];
+	}
 }
