6cd0c7edd3d27402c8d547b792dca5211541b4ab
xiang-ys <525717458@qq.com>
2023-09-23 20:02:09 +0800

    完善

--- 学习c++/学习c++.cpp
+++ 学习c++/学习c++.cpp
@@ -1,6 +1,7 @@
 ﻿//// 学习数据库c++.cpp
 #include<stdlib.h>
 #include<iostream>
+#include<Windows.h>
 #define ElemType int    //初始化数据类型 
 #define listsize_MAX 100//初始空间分配量
 #define ok 1
@@ -17,34 +18,58 @@ int InitList(SqList& L);//线性表初始化
 void input(SqList& L, int num);//添加数据 ,添加次数为num
 void ListOutput(SqList& L);//输出数据
 void InsertData(SqList& L, int location, int num);//插入(Insert)数据
+int GetData(SqList& L, int location);//获得数据
 void DeleteData(SqList& L, int location);//删除数据
+int ListLength(SqList& L);//检查线性表有多少元素
+void DestroyList(SqList& L);//销毁线性表
+int badData(SqList& L,int num);//输入错误数据
 
 int main() {
 	int num;//数字
 	int location;//位置
+
 	SqList L1;//定义线性表
 	InitList(L1);//初始化
 	cout << "请输入你要添加多少个数据进去，不得超过100" << endl;
 	cin >> num;//你要添加多少个数据进去
+	if (num > 100)exit(0);//防止溢出
 	input(L1, num);//添加数据
+	system("cls");
 	ListOutput(L1);//输出数据
-
+	cout << "现在输出线性表有" << ListLength(L1) << "元素" << endl;
+	cout << endl;//分割操作
 	cout << "现在执行插入数据处理，输入你要插入的位置和数据，空格隔开" << endl;
 	cin >> location >> num;
 	InsertData(L1, location, num);
+	badData(L1, location);//检查是否超范围
 	ListOutput(L1);//输出数据
-
+	cout << "现在输出线性表有" << ListLength(L1) << "元素" << endl;
+	cout << endl;//分割操作
+	cout << "现在执行获得特定位置的数据，请输入你想获得数据的位置" << endl;
+	cin >> num;
+	cout << "获得数据为：" << GetData(L1, num) << endl;
+	cout << endl;//分割操作
 	cout << "现在执行删除数据处理，输入你要删除的位置" << endl;
 	cin >> num;
 	DeleteData(L1, num);
+	badData(L1, num);//检查是否超范围
 	ListOutput(L1);//输出数据
+	cout << "现在输出线性表有" << ListLength(L1) << "元素" << endl;
+	cout << endl;//分割操作
+	cout << "现在执行销毁线性表，按Enter键执行" << endl;
+	system("pause");
+	system("cls");
+	DestroyList(L1); 
+	cout << "销毁成功！" << endl;
+	ListOutput(L1);//输出数据
+	cout << "现在输出线性表有"<< ListLength(L1) <<"元素"<<endl;
 	return 0;
 }
 
 
 int InitList(SqList& L) {//线性表初始化 
 	L.elem = (ElemType*)malloc(listsize_MAX * sizeof(ElemType));//malloc需要 #include<stdlid.h>
-	if (!L.elem)exit(0);//指针为NULL相当于指针等于零，所以L.elem==NULL则!L.elem为真
+	if (!L.elem)exit(0);//指针为NULL相当于指针等于零，所以L.elem==NULL则!L.elem为真。exit(0)退出程序
 	L.length = 0;
 	L.listsize = listsize_MAX;
 	return ok;
@@ -59,7 +84,7 @@ void input(SqList& L, int num) {//添加数据 ,添加次数为num
 }
 
 void ListOutput(SqList& L) {//输出数据
-	cout << "输出数据" << endl;
+	cout << "输出数据：";
 	for (int i = 0; i < L.length; i++){//遍历线性表
 		cout << L.elem[i]<<'\t';
 	}
@@ -74,9 +99,51 @@ void InsertData(SqList& L,int location,int num) {//插入(Insert)数据
 	L.elem[location - 1] = num;//插入数据,减一是因为以位置开始数，不是索引
 }
 
+int GetData(SqList& L, int location) {//获得特定位置数据
+	while (1) {
+		if (L.length < location) {
+			cout << "获取失败，原因:超出范围！" << endl;
+		}
+		else
+		break;
+	}
+	return L.elem[location - 1];
+}
+
 void DeleteData(SqList& L,int location) {//删除数据
 	L.length--;
-	for (int i = location; i <=L.length; i++) {//在位置(location)后的数据全部往前移一位
+	for (int i = location; i <L.length; i++) {//在位置(location)后的数据全部往前移一位
 		L.elem[i - 1] = L.elem[i];
 	}
+}
+
+int ListLength(SqList& L) {//检查线性表有多少元素
+	int num = 0;
+	for (int i = 0; i < L.length; i++) {//遍历
+		if (L.elem[i] != NULL)
+			num++;
+	}
+	return num;
+}
+
+void DestroyList(SqList& L) {//销毁线性表
+	for (int i = 0; i < L.length; i++) {//遍历
+		if (L.elem[i] != NULL&&L.elem[i] != '0')L.elem[i] = NULL;
+	}
+	L.length = 0;
+}
+
+int badData(SqList& L,int num) {//输入错误数据处理
+	if (num > L.length) {
+		system("cls");
+		cout << "你小子怎么能输入错误数据！！！！" << endl;
+		Sleep(500);
+		cout << "倒计时10秒后自动爆炸qwq" << endl;
+		for (int i = 1; i <= 10; i++) {
+			cout << i << "秒" << endl;
+			Sleep(1000);
+		}
+		exit(0);
+	}
+	return ok;
 }
