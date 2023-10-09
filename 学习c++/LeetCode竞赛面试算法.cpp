ece056517a6042302871cc11f721c0dc7f137050
xiang-ys <525717458@qq.com>
2023-10-09 23:11:34 +0800

    增加对数器

--- 学习c++/LeetCode竞赛面试算法.cpp
+++ 学习c++/LeetCode竞赛面试算法.cpp
@@ -1,16 +1,33 @@
 #include<iostream>
+#include<random>
+#include <chrono>  // 用于高分辨率时钟
+#include<vector>
+#include<time.h>
+#include<stdlib.h>
 using namespace std;
 
 void swap(int a, int b);//使用异或交换a，b位置(抖机灵算法)
 int printOddTimeNum1();//有一个数字在数组出现奇数次，其他都出现偶数次,求奇数
 int printOddTimeNum2();//有两个数字在数组出现奇数次，其他都出现偶数次,求奇数
+int generateRandomArray(vector<int>& vec,int maxSize,int maxValue);//对数器
+template<typename T>
+void generateRandomVector(std::vector<T>& vec, int maxSize, T minValue, T maxValue);//大佬的对数器
+
 int main() {
-	int a, b;
+	/*int a, b;
 	cout << "请输入两个数，将使用异或进行交换" << endl;
 	cin >> a >> b;
 	swap(a, b);
 	cout << printOddTimeNum1()<<endl;
-	printOddTimeNum2();
+	printOddTimeNum2();*/
+	vector<int> vec1;
+	for (int i = 0; i < 100; i++) {
+		generateRandomArray(vec1, 10, 50);
+		for (int num : vec1) {
+			cout << num << " ";
+		}
+		cout << endl;
+	}
 	return 0;
 }
 
@@ -53,4 +70,29 @@ int printOddTimeNum2() {
 	cout << onlyOne << " " << (eor ^ onlyOne);
 	
 	return eor;
+}
+
+int generateRandomArray(vector<int>& vec,int maxSize, int maxValue) {//我自个儿写的对数器，只不过是对大佬的拙劣模仿罢了
+	unsigned seed = chrono::high_resolution_clock::now().time_since_epoch().count();
+	static default_random_engine e(seed);
+	static uniform_int_distribution<unsigned> u(0, maxValue);//这个是随机数引擎生成随机数
+	int size = u(e);
+	vec = vector<int>(size, -1);//长度为size，所有位置赋值-1
+	for (auto it = vec.begin(); it != vec.end(); ++it) {//it是一个迭代器，它在循环中移动，指向 vec 的每一个元素。
+		*it = u(e);
+		/* *it 用于访问 it 当前指向的元素的值。*/
+	}
+	return 0;
+}
+
+template<typename T>
+void generateRandomVector(vector<T>& vec, int maxSize, T minValue, T maxValue) {//别的大佬写的对数器
+	assert(maxValue > minValue);    // assert
+	srand((unsigned int)time(nullptr)); // include<ctime>
+	int size = rand() % maxSize + 1;  // [1, maxsize]
+	vec = std::vector<T>(size, -1);   //initialization with -1
+	for (auto it = vec.begin(); it != vec.end(); ++it) {
+		*it = rand() % (maxValue - minValue) + minValue + 1; // [minvalue + 1, maxValue]
+	}
+	return;
 }
