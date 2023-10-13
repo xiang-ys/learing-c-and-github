6b326bbff7bbd5747b3ab879e3427525ed57ff4c
xiang-ys <525717458@qq.com>
2023-10-13 12:47:42 +0800

    添加归并算法

--- 学习c++/LeetCode竞赛面试算法.cpp
+++ 学习c++/LeetCode竞赛面试算法.cpp
@@ -1,7 +1,8 @@
 #include<iostream>
 #include<random>
 #include <chrono>  // 用于高分辨率时钟
-#include<vector>
+#include<vector>   //数组，但是可以自定义长度
+#include<algorithm>
 #include<time.h>
 #include<stdlib.h>
 using namespace std;
@@ -9,7 +10,10 @@ using namespace std;
 void swap(int a, int b);//使用异或交换a，b位置(抖机灵算法)
 int printOddTimeNum1();//有一个数字在数组出现奇数次，其他都出现偶数次,求奇数
 int printOddTimeNum2();//有两个数字在数组出现奇数次，其他都出现偶数次,求奇数
-int generateRandomArray(vector<int>& vec,int maxSize,int maxValue);//对数器
+int generateRandomArray(vector<int>& vec,int maxSize,int maxValue);//对数器,maxSize最大长度，maxValue最大数据
+int process(vector<int>& vec, int L, int R);//用递归在vec数组上求最大值,用来理解归并排序
+int process_super(vector<int>& vec, int L, int R);
+void merge(vector<int>& vec,int L,int M,int R);
 template<typename T>
 void generateRandomVector(std::vector<T>& vec, int maxSize, T minValue, T maxValue);//大佬的对数器
 
@@ -20,13 +24,17 @@ int main() {
 	swap(a, b);
 	cout << printOddTimeNum1()<<endl;
 	printOddTimeNum2();*/
-	vector<int> vec1;
-	for (int i = 0; i < 100; i++) {
-		generateRandomArray(vec1, 10, 50);
-		for (int num : vec1) {
+	vector<int> vec1,vec2;
+	for (int i = 0; i < 1000; i++) {
+		generateRandomArray(vec1, 10, 20);
+		vec2 = vector<int>(vec1.size(), -1);
+		copy(vec1.begin(), vec1.end(), vec2.begin());//vec2复制vec1
+		process_super(vec1, 0, vec1.size() - 1);
+		for (int num : vec1) {//遍历vec1
 			cout << num << " ";
 		}
-		cout << endl;
+		/*cout << "最大值为"<<process(vec1, 0, vec1.size()-1);*/
+		cout << "\n";
 	}
 	return 0;
 }
@@ -75,8 +83,9 @@ int printOddTimeNum2() {
 int generateRandomArray(vector<int>& vec,int maxSize, int maxValue) {//我自个儿写的对数器，只不过是对大佬的拙劣模仿罢了
 	unsigned seed = chrono::high_resolution_clock::now().time_since_epoch().count();
 	static default_random_engine e(seed);
-	static uniform_int_distribution<unsigned> u(0, maxValue);//这个是随机数引擎生成随机数
-	int size = u(e);
+	static uniform_int_distribution<unsigned> u(1, maxValue);//这个是随机数引擎生成随机数,限制最大数值
+	static uniform_int_distribution<unsigned> n(1, maxSize); //限制最大长度
+	int size = n(e);
 	vec = vector<int>(size, -1);//长度为size，所有位置赋值-1
 	for (auto it = vec.begin(); it != vec.end(); ++it) {//it是一个迭代器，它在循环中移动，指向 vec 的每一个元素。
 		*it = u(e);
@@ -95,4 +104,37 @@ void generateRandomVector(vector<T>& vec, int maxSize, T minValue, T maxValue) {
 		*it = rand() % (maxValue - minValue) + minValue + 1; // [minvalue + 1, maxValue]
 	}
 	return;
+}
+//vec[L..R]范围求最大值
+int process(vector<int>& vec, int L, int R) {
+	if (L == R) return vec[L];
+	int mid = L + ((R - L)>> 1);//中点，右移一位等于除于2
+	int leftMax = process(vec, L, mid);
+	int rightMax = process(vec, mid + 1, R);
+	return leftMax < rightMax ? rightMax : leftMax;
+}
+//vec[L..R]范围排序
+int process_super(vector<int>& vec, int L, int R) {
+	if (L == R) return vec[L];
+	int mid = L + ((R - L) >> 1);//中点，右移一位等于除于2
+	process(vec, L, mid);
+	process(vec, mid + 1, R);
+	merge(vec, L, mid, R);
+}
+//归并排序主操作
+void merge(vector<int>& vec, int L, int M, int R) {
+	vector<int> vecHelp(vec.size(), -1);
+	auto it = vecHelp.begin();
+	int p1 = L;
+	int p2 = M + 1;
+	while (p1 <= M && p2 <= R){
+		*(it++) = vec[p1++] <= vec[p2++] ? vec[p2++] : vec[p1++];
+	}
+	while (p1 <= M){
+		*(it++) = vec[p2++];
+	}
+	while (p2 <= R) {
+		*(it++) = vec[p1++];
+	}
+	copy(vecHelp.begin(), vecHelp.end(), vec.begin());
 }
--- 学习c++/双向链表.cpp
+++ 学习c++/双向链表.cpp
@@ -1,105 +1,106 @@
-#include<iostream>
-#include<stdlib.h>
-#define ElemType int
-#define ok 1
-#define on 0
-using namespace std;
-
-typedef struct DoubleNode {//定义双链表
-	ElemType data;
-	DoubleNode* next;
-	DoubleNode* prior;
-}DuLNode, * DuLinkList;
-
-int InitList(DuLinkList& L);//初始化双链表
-int input(DuLinkList& L, int size);//添加数据
-int ListTraver(DuLinkList& L);//遍历双链表同时计算长度 
-int ListInsert(DuLinkList& L, int num, int data);//插入操作 
-int ListDelete(DuLinkList& L, int size);//删除操作 
-
-int main() {
-	ElemType num, size;
-	DuLinkList L1;
-	InitList(L1);
-	cout << "已经创建双链表，请输入你要输入的数据长度" << endl;
-	cin >> size;
-	input(L1, size);
-	cout << "长度为" << ListTraver(L1) << endl;//遍历操作，同时输出长度 
-
-	cout << "现在进行插入操作，请输入你要插入的数字和位置，按顺序" << endl;
-	cin >> num >> size;
-	ListInsert(L1, num, size);
-	cout << "长度为" << ListTraver(L1) << endl;//遍历操作，同时输出长度 
-
-	cout << "现在进行删除操作，请输入你要删除的位置" << endl;
-	cin >> size;
-	ListDelete(L1, size);
-	cout << "长度为" << ListTraver(L1) << endl;//遍历操作，同时输出长度 
-	return 0;
-}
-
-int InitList(DuLinkList& L) {//初始化双链表
-	L = (DuLinkList)malloc(sizeof(DuLNode));
-	if (!L)exit(0);
-	L->next = L;//头结点的下一个指向自己 
-	L->prior = L;//头结点的上一个指向自己 
-	return ok;
-}
-
-int input(DuLinkList& L, int size) {//添加数据
-	ElemType n;
-	DuLinkList p, L1;
-	p = L;
-	for (int i = 0; i < size; i++) {
-		L1 = (DuLinkList)malloc(sizeof(DuLNode));
-		cout << "请输入数字" << endl;
-		cin >> n;
-		L1->data = n;//先将数据存入L1 
-		p->next = L1;//p的下一个指向L1 
-		L1->next = L1;//L1的下一个没得指了，指向自己也行.
-		L1->prior = p;//L1的上一个指向p 
-		p = p->next;//p指向下一个，以继续完成下一步的添加数据
-	}
-	return ok;
-}
-
-int ListTraver(DuLinkList& L) {//遍历双链表同时计算长度 
-	int size = 0;//计算 
-	DuLinkList p;
-	p = L;
-	while ((p->next) != p) {//直到p的下一个指向自己的时候结束 
-		p = p->next;
-		cout << p->data << " ";
-		size++;
-	}
-	return size;
-}
-
-int ListInsert(DuLinkList& L, int num, int data) {//插入操作 
-	DuLinkList p, L1;
-	p = L;
-	for (int i = 1; i < num; i++) {//让p循环到我们要插入的节点的前一个位置 
-		p = p->next;
-	}
-	L1 = (DuLinkList)malloc(sizeof(DuLNode));
-	L1->data = data;//存入数据 
-	L1->next = p->next;//L1的下一个指向p的下一个 
-	p->next = L1;//p的下一个指向L1 
-	L1->prior = p;//L1的上一个指向p 
-	L1->next->prior = L1; //L1的下一个的上一个指向L1,构成双向链表 
-	return ok;
-}
-
-int ListDelete(DuLinkList& L, int size) {//删除操作 
-	DuLinkList Q, p;
-	p = L->next;
-	Q = p;
-	for (int i = 1; i < size; i++) {//让p循环到我们要插入的节点位置，Q为前一个q前一个节点 
-		Q = p;
-		p = p->next;
-	}
-	Q->next = p->next; //Q的下一个指向p的下一个，完成删除，没有指针指向p 
-	Q->next->prior = Q; //让Q的下一个的上一个指向Q，完成双链表 
-	delete(p);//删除节点，减少占用 
-	return ok;
-}
+//#include<iostream>
+//#include<stdlib.h>
+//#define ElemType int
+//#define ok 1
+//#define on 0
+//using namespace std;
+//
+//typedef struct DoubleNode {//定义双链表
+//	ElemType data;
+//	DoubleNode* next;
+//	DoubleNode* prior;
+//}DuLNode, * DuLinkList;
+//
+//int InitList(DuLinkList& L);//初始化双链表
+//int input(DuLinkList& L, int size);//添加数据
+//int ListTraver(DuLinkList& L);//遍历双链表同时计算长度 
+//int ListInsert(DuLinkList& L, int num, int data);//插入操作 
+//int ListDelete(DuLinkList& L, int size);//删除操作 
+//
+//int main() {
+//	ElemType num, size;
+//	DuLinkList L1;
+//	InitList(L1);
+//	cout << "已经创建双链表，请输入你要输入的数据长度" << endl;
+//	cin >> size;
+//	input(L1, size);
+//	cout << "长度为" << ListTraver(L1) << endl;//遍历操作，同时输出长度 
+//
+//	cout << "现在进行插入操作，请输入你要插入的数字和位置，按顺序" << endl;
+//	cin >> num >> size;
+//	ListInsert(L1, num, size);
+//	cout << "长度为" << ListTraver(L1) << endl;//遍历操作，同时输出长度 
+//
+//	cout << "现在进行删除操作，请输入你要删除的位置" << endl;
+//	cin >> size;
+//	ListDelete(L1, size);
+//	cout << "长度为" << ListTraver(L1) << endl;//遍历操作，同时输出长度 
+//	return 0;
+//}
+//
+//int InitList(DuLinkList& L) {//初始化双链表
+//	L = (DuLinkList)malloc(sizeof(DuLNode));
+//	if (!L)exit(0);
+//	L->next = L;//头结点的下一个指向自己 
+//	L->prior = L;//头结点的上一个指向自己 
+//	return ok;
+//}
+//
+//int input(DuLinkList& L, int size) {//添加数据
+//	ElemType n;
+//	DuLinkList p, L1;
+//	p = L;
+//	for (int i = 0; i < size; i++) {
+//		L1 = (DuLinkList)malloc(sizeof(DuLNode));
+//		cout << "请输入数字" << endl;
+//		cin >> n;
+//		L1->data = n;//先将数据存入L1 
+//		p->next = L1;//p的下一个指向L1 
+//		L1->next = L1;//L1的下一个没得指了，指向自己也行.
+//		L1->prior = p;//L1的上一个指向p 
+//		p = p->next;//p指向下一个，以继续完成下一步的添加数据
+//	}
+//	return ok;
+//}
+//
+//int ListTraver(DuLinkList& L) {//遍历双链表同时计算长度 
+//	int size = 0;//计算 
+//	DuLinkList p;
+//	p = L;
+//	while ((p->next) != p) {//直到p的下一个指向自己的时候结束 
+//		p = p->next;
+//		cout << p->data << " ";
+//		size++;
+//	}
+//	return size;
+//}
+//
+//int ListInsert(DuLinkList& L, int num, int data) {//插入操作 
+//	DuLinkList p, L1;
+//	p = L;
+//	for (int i = 1; i < num; i++) {//让p循环到我们要插入的节点的前一个位置 
+//		p = p->next;
+//	}
+//	L1 = (DuLinkList)malloc(sizeof(DuLNode));
+//	L1->data = data;//存入数据 
+//	L1->next = p->next;//L1的下一个指向p的下一个 
+//	p->next = L1;//p的下一个指向L1 
+//	L1->prior = p;//L1的上一个指向p 
+//	L1->next->prior = L1; //L1的下一个的上一个指向L1,构成双向链表 
+//	return ok;
+//}
+//
+//int ListDelete(DuLinkList& L, int size) {//删除操作 
+//	DuLinkList Q, p;
+//	p = L->next;
+//	Q = p;
+//	for (int i = 1; i < size; i++) {//让p循环到我们要删除的节点位置，Q为前一个q前一个节点 
+//		Q = p;
+//		p = p->next;
+//	}
+//	Q->next = p->next; //Q的下一个指向p的下一个，完成删除，没有指针指向p 
+//	Q->next->prior = Q; //让Q的下一个的上一个指向Q，完成双链表 
+//	delete(p);//删除节点，减少占用 
+//	return ok;
+//}
+//
